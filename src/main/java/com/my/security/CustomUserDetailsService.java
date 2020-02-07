dff6dd7c7dc3e9583fd58fdb8c0e68f5ca259622
maoyu <maoyu@halosee.com>
2020-02-07 13:25:26 +0800

    自定义userDetailsService

--- src/main/java/com/my/security/CustomUserDetailsService.java
+++ src/main/java/com/my/security/CustomUserDetailsService.java
@@ -9,11 +9,11 @@ import com.my.service.SysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.security.core.userdetails.User;
 
 import java.util.ArrayList;
 import java.util.Collection;
