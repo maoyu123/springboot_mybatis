eab4c55f1478433894167aaa0de3b22860279171
maoyu <maoyu@halosee.com>
2020-02-17 23:03:59 +0800

    增加SysPermissionService 权限service接口

---
+++ src/main/java/com/my/service/SysPermissionService.java
@@ -0,0 +1,20 @@
+package com.my.service;
+import com.my.entity.SysPermission;
+import com.my.mapper.SysPermissionMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SysPermissionService {
+    @Autowired
+    private SysPermissionMapper permissionMapper;
+
+    /**
+     * 获取指定角色所有权限
+     */
+    public List<SysPermission> listByRoleId(Integer roleId) {
+        return permissionMapper.listByRoleId(roleId);
+    }
+}
